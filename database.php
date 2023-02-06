32dab69074a3912ff90bd516fc03841395cc51e3
lollomrt <lorenzomartini.mrt@outlook.it>
2023-02-06 22:53:36 +0100

    mancano soltanto gli ultimi due elementi di ogni card

--- database.php
+++ database.php
@@ -7,16 +7,14 @@
 
     
 
-    $prodotto_1 = new cibo ('immagine1', 'Royal Canin Mini Adult', '43,99€', new categoria ('fa-solid fa-dog', 'cane'), '545g', ['prosciutto', 'riso']);
-    $prodotto_2 = new cibo ('immagine1', 'halmo nature olistic aintenance medium large tonno e riso', '44,99€', new categoria ('fa-solid fa-dog', 'cane'), '600g', ['manzo', 'cereali']);
-    $prodotto_3 = new cibo ('immagine1', 'halmo nature cat daily lattina', '34,99€', new categoria ('fa-solid fa-cat', 'gatto'), '400g', ['tonno', 'pollo', 'prosciutto']);
-    $prodotto_4 = new cibo ('immagine1', 'mangime per pesci guppy in fiocchi', '2,95€', new categoria ('fa-solid fa-fish', 'pesce'), '30g', ['pesci e sottoprodotti dei pesci', 'cereali', 'lieviti', 'alghe']);
-    $prodotto_5 = new accessorio ('immagine2', 'voliera wilma in legno', '184,99€', new categoria ('fa-solid fa-dove', 'uccello'), 'legno', 'M: L 83 x P 67 x H 153 cm');
-    $prodotto_6 = new accessorio ('immagine2', 'cartucce filtranti per filtro easyCrystal', '2,29€', new categoria ('fa-solid fa-fish', 'fish'), 'materiale espanso', 'ND');
-    $prodotto_7 = new gioco ('immagine1', 'kong classic', '13,49€', new categoria ('fa-solid fa-dog', 'cane'), 'galleggia e rimbalza', '8,5 cm x 10 cm');
-    $prodotto_8 = new gioco ('immagine1', 'topini di peluche trixie', '4,99€', new categoria ('fa-solid fa-cat', 'gatto'), 'morbido con codina in corda', '8,5 cm x 10 cm');
+    $prodotto_1 = new cibo ('https://www.amoreanimaleshop.it/wp-content/uploads/2021/05/Royal-Canin-Dog-Mini-Adult.jpg', 'Royal Canin Mini Adult', '43,99€', new categoria ('fa-solid fa-dog', 'cane'), '545g', ['prosciutto', 'riso']);
+    $prodotto_2 = new cibo ('https://www.bauzaar.it/media/catalog/product/t/o/tonno_e_riso_cane_1.jpg?width=700&height=700&store=default&image-type=image', 'halmo nature olistic aintenance medium large tonno e riso', '44,99€', new categoria ('fa-solid fa-dog', 'cane'), '600g', ['manzo', 'cereali']);
+    $prodotto_3 = new cibo ('https://www.amoreanimaleshop.it/wp-content/uploads/2021/02/Almo-Nature-Cat-Daily-Manzo-400.jpg', 'halmo nature cat daily lattina', '34,99€', new categoria ('fa-solid fa-cat', 'gatto'), '400g', ['tonno', 'pollo', 'prosciutto']);
+    $prodotto_4 = new cibo ('https://www.amoreanimaleshop.it/wp-content/uploads/2021/04/Tetra-Guppy-Mini-Flakes.jpg', 'mangime per pesci guppy in fiocchi', '2,95€', new categoria ('fa-solid fa-fish', 'pesce'), '30g', ['pesci e sottoprodotti dei pesci', 'cereali', 'lieviti', 'alghe']);
+    $prodotto_5 = new accessorio ('https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg?v=1760290610', 'voliera wilma in legno', '184,99€', new categoria ('fa-solid fa-dove', 'uccello'), 'legno', 'M: L 83 x P 67 x H 153 cm');
+    $prodotto_6 = new accessorio ('https://www.aquariumline.com/catalog/images/15412_Iy7JqHVtJLbq.jpg', 'cartucce filtranti per filtro easyCrystal', '2,29€', new categoria ('fa-solid fa-fish', 'fish'), 'materiale espanso', 'ND');
+    $prodotto_7 = new gioco ('https://www.naturepetshop.it/wp-content/uploads/T1_1_1000x1000.jpg', 'kong classic', '13,49€', new categoria ('fa-solid fa-dog', 'cane'), 'galleggia e rimbalza', '8,5 cm x 10 cm');
+    $prodotto_8 = new gioco ('https://www.zampashop.com/9326-large_default/topo-gioco-per-gatti-con.jpg', 'topini di peluche trixie', '4,99€', new categoria ('fa-solid fa-cat', 'gatto'), 'morbido con codina in corda', '8,5 cm x 10 cm');
 
-    $prodotti = [$prodotto_1, $prodotto_2, $prodotto_3, $prodotto_4, $prodotto_5, $prodotto_6, $prodotto_7, $prodotto_8]
-
-
+    $prodotti = [$prodotto_1, $prodotto_2, $prodotto_3, $prodotto_4, $prodotto_5, $prodotto_6, $prodotto_7, $prodotto_8];
 ?>
--- index.php
+++ index.php
@@ -11,16 +11,37 @@
     <title>PHP OOP2</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <div>
-        <pre>
-        <?php
-           var_dump($prodotto_1);
-           var_dump($prodotto_2);
-           var_dump($prodotto_3);
-        ?>
-        </pre>
+        <div class="container-fluid my-5">
+            <div class="row">
+                <div class="col">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col">
+                                <h1 class="my-5">I nostri prodotti</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container w-75 d-flex flex-wrap gap-1rem ">
+                        <?php foreach($prodotti as $elem) { ?>
+                            <div class="card w-3">
+                                <img class="card-img-top" src="<?php echo $elem->immagine ?>" alt="Card image cap">
+                                <div class="card-body">
+                                    <h1><?php echo $elem->titolo ?></h1>
+                                    <span><i class="<?php echo $elem->categoria->icona ?>"></i><?php echo $elem->categoria->specie ?></span>
+                                    <p><strong>prezzo:</strong> <?php echo $elem->prezzo ?></p>
+                                    <p><strong>prezzo:</strong> <?php echo $elem->prezzo ?></p>
+                                </div>
+                            </div>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
