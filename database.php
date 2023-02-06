dd73cc9bf19cb940690943e7efbd68c57e845f98
lollomrt <lorenzomartini.mrt@outlook.it>
2023-02-06 18:09:59 +0100

    Logica completa - ora devo graficizzare tutto

--- database.php
+++ database.php
@@ -1,8 +1,13 @@
 <?php
     include __DIR__.'./models/categoria.php';
-    include __DIR__.'./models/prodotti.php';
+    include __DIR__.'./models/prodotto.php';
     include __DIR__.'./models/gioco.php';
+    include __DIR__.'./models/cibo.php';
+    include __DIR__.'./models/accessorio.php';
 
+    $prodotto_1 = new gioco ('immagine1', 'titolo1', 'prezzo1', new categoria ('immagineCane', 'testospecie'), 'caratteristiche', 'dimensioni');
+    $prodotto_2 = new cibo ('immagine2', 'titolo2', 'prezzo2', new categoria ('immagineCane', 'testospecie'), 'peso netto', ['olio', 'sale', 'pepe']);
+    $prodotto_3 = new accessorio ('immagine2', 'titolo2', 'prezzo2', new categoria ('immagineCane', 'testospecie'), 'materiale', ['h5', 'l2', 'pd']);
 
-    $prodotto_1 = new prodotti ('immagine1', 'titolo1', 'prezzo1', new categoria ('immagineCane', 'testospecie'), 'caratteristiche', 'dimensioni')
+    
 ?>
