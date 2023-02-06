b9aa4d7544cdd74c35f2f28060020d563c92d3ab
lollomrt <lorenzomartini.mrt@outlook.it>
2023-02-06 17:30:38 +0100

    Prova costruzione sotto classe

--- database.php
+++ database.php
@@ -1,7 +1,8 @@
 <?php
     include __DIR__.'./models/categoria.php';
     include __DIR__.'./models/prodotti.php';
-    
+    include __DIR__.'./models/gioco.php';
 
-    $prodotto_1 = new prodotti ('immagine1', 'titolo1', 'prezzo1', new categoria ('immagineCane', 'testospecie'))
+
+    $prodotto_1 = new prodotti ('immagine1', 'titolo1', 'prezzo1', new categoria ('immagineCane', 'testospecie'), 'caratteristiche', 'dimensioni')
 ?>
---
+++ models/gioco.php
@@ -0,0 +1,12 @@
+<?php
+    class gioco extends prodotti {
+        public $caratteristiche;
+        public $dimensioni;
+
+        public function __construct($_immagine, $_titolo, $_prezzo, categoria $_categoria, $_caratteristiche, $__dimensioni){
+            parent:: __construct($_immagine, $_titolo, $_prezzo, $_categoria);
+            $this->caratteristiche = $_caratteristiche;
+            $this->dimensioni = $__dimensioni;
+        }
+    }
+?>
